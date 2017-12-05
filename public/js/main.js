22738c78cf3f00a7a1d788d645e663bc0e672dc5
usergnavarro <navarro.gabriella@gmail.com>
2017-12-05 18:51:15 -0200

    insere o typewriter no header

--- public/js/main.js
+++ public/js/main.js
@@ -114,3 +114,13 @@
 
   return ptBr;
 })));
+
+function typeWritter(texto,idElemento,tempo){
+    var char = texto.split('').reverse();
+    var typer = setInterval(function () {
+        if (!char.length) return clearInterval(typer);
+        var next = char.pop();
+        document.getElementById(idElemento).innerHTML += next;
+    }, tempo);
+}
+typeWritter('<!-- confira os nossos eventos -->','log',65);
